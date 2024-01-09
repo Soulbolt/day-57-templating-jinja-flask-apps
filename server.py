c3530af5de0e99d9dac6ffd212f33ea877193e29
Soulbolt <soulbolt@gmail.com>
2024-01-09 11:51:06 -0700

    created code and html file to render some baisc tags and use apis to obtain data and display on guess.html using jinja templates

--- server.py
+++ server.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
 import random
+import datetime
+import requests
+import json
 
+GENDERIZE_API_ENDPOINT = "https://api.genderize.io?name="
+AGIFY_API_ENDPOINT = "https://api.agify.io?name="
+BLOG_API_ENDPOINT = "https://api.npoint.io/5abcca6f4e39b4955965"
+# name = "Cesar"
+# response = requests.get(url=GENDERIZE_API_URL+name)
+# response.raise_for_status
+# gender = json.loads(response.content)
+# print(gender["name"])
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    current_year = datetime.date.today().year
+    return render_template("index.html", num=random_number, year=current_year)  # We can pass python variables like num=random_number
 
+@app.route("/guess/<name>")
+def guess_name(name):
+    genderize_response = requests.get(url=GENDERIZE_API_ENDPOINT+name)
+    genderize_response.raise_for_status
+    gender = json.loads(genderize_response.content)["gender"]
+    agify_response = requests.get(url=AGIFY_API_ENDPOINT+name)
+    agify_response.raise_for_status
+    age = json.loads(agify_response.content)["age"]
+    return render_template("guess.html", name=name, gender=gender, age=age)
+
+@app.route("/blog")
+def blog():
+    response = requests.get(BLOG_API_ENDPOINT)
+    response.raise_for_status
+    all_post = response.json()
+    return render_template("blog.html", posts=all_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
